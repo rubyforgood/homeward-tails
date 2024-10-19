eccba8bbae2d229976e992698bfe912c18deb38e
mononoken <ken.maeshima@pm.me>
2024-10-19 15:39:59 -0700

    Fix incorrect usage of tenancy in test

--- test/integration/organizations/staff/adoption_application_reviews_test.rb
+++ test/integration/organizations/staff/adoption_application_reviews_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class AdoptionApplicationReviewsTest < ActionDispatch::IntegrationTest
+class Organizations::Staff::AdoptionApplicationReviewsTest < ActionDispatch::IntegrationTest
   setup do
     @form_submission = create(:form_submission)
     @awaiting_review_app = create(:adopter_application, status: :awaiting_review, form_submission: @form_submission)
@@ -9,9 +9,7 @@ class AdoptionApplicationReviewsTest < ActionDispatch::IntegrationTest
     @withdrawn_app = create(:adopter_application, :withdrawn, form_submission: @form_submission)
     @successful_applicant_app = create(:adopter_application, status: :successful_applicant, form_submission: @form_submission)
     @adoption_made_app = create(:adopter_application, status: :adoption_made, form_submission: @form_submission)
-    @organization = create(:organization)
-    @custom_page = create(:custom_page, organization: @organization)
-    Current.organization = @organization
+    @custom_page = create(:custom_page, organization: ActsAsTenant.current_tenant)
   end
 
   context "non-staff" do
