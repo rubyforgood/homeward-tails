aa9e57e2f8c4c15026daa53e126869d97bf70df3
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-10-19 12:58:26 -0400

    use module

--- app/controllers/organizations/adoptable_pets_controller.rb
+++ app/controllers/organizations/adoptable_pets_controller.rb
@@ -1,59 +1,61 @@
-class Organizations::AdoptablePetsController < Organizations::BaseController
-  include ::Pagy::Backend
+module Organizations
+  class AdoptablePetsController < Organizations::BaseController
+    include ::Pagy::Backend
 
-  skip_before_action :authenticate_user!
-  skip_verify_authorized only: %i[index]
-  before_action :set_likes, only: %i[index show],
-    if: -> { allowed_to?(:index?, Like) }
-  helper_method :get_animals
+    skip_before_action :authenticate_user!
+    skip_verify_authorized only: %i[index]
+    before_action :set_likes, only: %i[index show],
+      if: -> { allowed_to?(:index?, Like) }
+    helper_method :get_animals
 
-  def index
-    @q = authorized_scope(
-      case params[:species]
-      when "dog"
-        Pet.Dog.unadopted
-      when "cat"
-        Pet.Cat.unadopted
-      else
-        redirect_back_or_to root_path and return
-      end,
-      with: Organizations::AdoptablePetPolicy
-    ).ransack(params[:q])
+    def index
+      @q = authorized_scope(
+        case params[:species]
+        when "dog"
+          Pet.Dog.unadopted
+        when "cat"
+          Pet.Cat.unadopted
+        else
+          redirect_back_or_to root_path and return
+        end,
+        with: Organizations::AdoptablePetPolicy
+      ).ransack(params[:q])
 
-    @pagy, paginated_adoptable_pets = pagy(
-      @q.result.includes(:adopter_applications, :matches, images_attachments: :blob),
-      limit: 9
-    )
+      @pagy, paginated_adoptable_pets = pagy(
+        @q.result.includes(:adopter_applications, :matches, images_attachments: :blob),
+        limit: 9
+      )
 
-    @pets = paginated_adoptable_pets
-  end
-
-  def show
-    @adoptable_pet_info = CustomPage.first&.adoptable_pet_info
-    @pet = Pet.find(params[:id])
-    authorize! @pet, with: Organizations::AdoptablePetPolicy
-
-    if current_user&.latest_form_submission
-      @adoption_application =
-        AdopterApplication.find_by(
-          pet_id: @pet.id,
-          form_submission_id: current_user.latest_form_submission.id
-        ) ||
-        @pet.adopter_applications.build(
-          form_submission: current_user.latest_form_submission
-        )
+      @pets = paginated_adoptable_pets
     end
-  end
 
-  private
+    def show
+      @adoptable_pet_info = CustomPage.first&.adoptable_pet_info
+      @pet = Pet.find(params[:id])
+      authorize! @pet, with: Organizations::AdoptablePetPolicy
 
-  def get_animals(species)
-    authorized_scope(Pet.where(species: species.capitalize).distinct.order(:breed),
-      with: Organizations::AdoptablePetPolicy).pluck(:breed)
-  end
+      if current_user&.latest_form_submission
+        @adoption_application =
+          AdopterApplication.find_by(
+            pet_id: @pet.id,
+            form_submission_id: current_user.latest_form_submission.id
+          ) ||
+          @pet.adopter_applications.build(
+            form_submission: current_user.latest_form_submission
+          )
+      end
+    end
 
-  def set_likes
-    likes = current_user.person.likes
-    @likes_by_id = likes.index_by(&:pet_id)
+    private
+
+    def get_animals(species)
+      authorized_scope(Pet.where(species: species.capitalize).distinct.order(:breed),
+        with: Organizations::AdoptablePetPolicy).pluck(:breed)
+    end
+
+    def set_likes
+      likes = current_user.person.likes
+      @likes_by_id = likes.index_by(&:pet_id)
+    end
   end
 end
