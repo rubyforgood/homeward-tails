3b20969b10dc0da3cb025ce19f5465b317e259e6
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-10-21 19:21:23 -0400

    update test

--- test/controllers/organizations/adoptable_pets_controller_test.rb
+++ test/controllers/organizations/adoptable_pets_controller_test.rb
@@ -28,16 +28,16 @@ module Organizations
         assert assigns[:pets].pluck(:id).exclude?(adopted_pet.id)
       end
 
-    should "render information on pets" do
-      get adoptable_pets_url
+      should "render information on pets" do
+        get adoptable_pets_url(species: "dog")
 
-      assert_select(".card li:nth-of-type(1)", text: /#{@pet.name}\s+#{@pet.species}/)
-      assert_select(".card li:nth-of-type(2)", text: /Age/)
-      assert_select(".card li:nth-of-type(3)", text: "Breed: #{@pet.breed}")
-      assert_select(".card li:nth-of-type(4)", text: "Sex: #{@pet.sex}")
-      assert_select(".card li:nth-of-type(5)", text: "Weight range: #{@pet.weight_from}-#{@pet.weight_to} #{@pet.weight_unit}")
+        assert_select(".card li:nth-of-type(1)", text: /#{@pet.name}\s+#{@pet.species}/)
+        assert_select(".card li:nth-of-type(2)", text: /Age/)
+        assert_select(".card li:nth-of-type(3)", text: "Breed: #{@pet.breed}")
+        assert_select(".card li:nth-of-type(4)", text: "Sex: #{@pet.sex}")
+        assert_select(".card li:nth-of-type(5)", text: "Weight range: #{@pet.weight_from}-#{@pet.weight_to} #{@pet.weight_unit}")
+      end
     end
-  end
 
     context "#show" do
       should "be authorized" do
