0e815a1e400ae4bb6758bb3e189ac03f388c9dea
Meri-MG <mgogi13@freeuni.edu.ge>
2024-10-31 09:46:02 +0400

    Comment out vars with locations

--- app/mailers/organization_account_requests_mailer.rb
+++ app/mailers/organization_account_requests_mailer.rb
@@ -4,9 +4,9 @@ class OrganizationAccountRequestsMailer < ApplicationMailer
     @requester_name = params[:organization_requester_name]
     @organization_phone = params[:phone_number]
     @organization_email = params[:email]
-    @country = params[:locations][:country]
-    @city_town = params[:locations][:city_town]
-    @province_state = params[:locations][:province_state]
+    # @country = params[:locations][:country]
+    # @city_town = params[:locations][:city_town]
+    # @province_state = params[:locations][:province_state]
 
     mail(from: "default@petrescue.org",
       to: "devs@email.com",
