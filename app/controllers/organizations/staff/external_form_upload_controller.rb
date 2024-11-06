d7b81b558d479120d737c8820cebf8e6e9808f93
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-11-06 16:17:52 -0500

    test deploy

--- app/controllers/organizations/staff/external_form_upload_controller.rb
+++ app/controllers/organizations/staff/external_form_upload_controller.rb
@@ -12,7 +12,7 @@ module Organizations
         authorize! :external_form_upload,
           context: {organization: Current.organization}
         file = params.require(:files).first
-        import_service = Organizations::Importers::GoogleCsvImportService.new(file)
+        xdas import_service = Organizations::Importers::GoogleCsvImportService.new(file)
         import_service.call
       end
     end
