c535901e51e3adf6a21bc91578bf99ee4c60dbbd
wandergithub <wandergonzalez0@hotmail.com>
2024-11-14 23:07:20 -0400

    Fix linter error

--- test/policies/organizations/activations_policy_test.rb
+++ test/policies/organizations/activations_policy_test.rb
@@ -2,75 +2,75 @@ require "test_helper"
 
 class Organizations::ActivationsPolicyTest < ActiveSupport::TestCase
   include PetRescue::PolicyAssertions
-  
+
   setup do
     @staff = create(:admin)
     @policy = -> { Organizations::ActivationsPolicy.new(@staff, user: @user) }
   end
 
   context "#update?" do
+    setup do
+      @action = -> { @policy.call.apply(:update?) }
+    end
+
+    context "when user is nil" do
       setup do
-        @action = -> { @policy.call.apply(:update?) }
+        @user = nil
       end
-  
-      context "when user is nil" do
+
+      should "return false" do
+        assert_equal false, @action.call
+      end
+    end
+
+    context "when user is adopter" do
+      setup do
+        @user = create(:adopter)
+      end
+
+      should "return false" do
+        assert_equal false, @action.call
+      end
+    end
+
+    context "when user is fosterer" do
+      setup do
+        @user = create(:fosterer)
+      end
+
+      should "return false" do
+        assert_equal false, @action.call
+      end
+    end
+
+    context "when user is staff" do
+      setup do
+        @user = create(:admin)
+      end
+
+      should "return false" do
+        assert_equal false, @action.call
+      end
+    end
+
+    context "when user is staff admin" do
+      setup do
+        @user = create(:super_admin)
+      end
+
+      should "return true" do
+        assert_equal true, @action.call
+      end
+
+      context "when staff is self" do
         setup do
-          @user = nil
+          @staff = @user
         end
-  
+
         should "return false" do
           assert_equal false, @action.call
-        end
-      end
-  
-      context "when user is adopter" do
-        setup do
-          @user = create(:adopter)
-        end
-  
-        should "return false" do
-          assert_equal false, @action.call
-        end
-      end
-  
-      context "when user is fosterer" do
-        setup do
-          @user = create(:fosterer)
-        end
-  
-        should "return false" do
-          assert_equal false, @action.call
-        end
-      end
-  
-      context "when user is staff" do
-        setup do
-          @user = create(:admin)
-        end
-  
-        should "return false" do
-          assert_equal false, @action.call
-        end
-      end
-  
-      context "when user is staff admin" do
-        setup do
-          @user = create(:super_admin)
-        end
-  
-        should "return true" do
-          assert_equal true, @action.call
-        end
-  
-        context "when staff is self" do
-          setup do
-            @staff = @user
-          end
-  
-          should "return false" do
-            assert_equal false, @action.call
-          end
         end
       end
     end
+  end
 end
