fe58a9bf7c8ebfa29cb46a8f94b1aa8dce711e66
Meri Gogichashvili <mgogi13@freeuni.edu.ge>
2024-11-16 23:46:29 +0400

    1009 direct cta buttons to form (#1109)
    * Rename button to Request an account
    
    * Add organization_requester_name column
    
    * Add OrganizationAccuntRequestController
    
    * Add a mailer
    
    * Comment out vars with locations
    
    * Change column name
    
    * Add OrganizationAccountRequest model
    
    * Fix external_form_url error
    
    * Address required changes
    
    * Merge with master

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,17 +2,17 @@
 #
 # Table name: organizations
 #
-#  id                :bigint           not null, primary key
-#  donation_url      :text
-#  email             :string           not null
-#  external_form_url :text
-#  facebook_url      :text
-#  instagram_url     :text
-#  name              :string           not null
-#  phone_number      :string
-#  slug              :string           not null
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
+#  id                          :bigint           not null, primary key
+#  donation_url                :text
+#  email                       :string           not null
+# external_form_url :text
+#  facebook_url                :text
+#  instagram_url               :text
+#  name                        :string           not null
+#  phone_number                :string
+#  slug                        :string           not null
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
 #
 # Indexes
 #
@@ -39,8 +39,6 @@ class Organization < ApplicationRecord
   has_many :locations
   accepts_nested_attributes_for :locations
 
-  before_save :normalize_phone
-
   validates :phone_number, phone: {possible: true, allow_blank: true}
   validates :name, presence: true
   validates :slug, presence: true, format: {with: /\A[a-zA-Z0-9_-]+\z/, message: "only allows letters, numbers, hyphens, and underscores"}
@@ -50,10 +48,4 @@ class Organization < ApplicationRecord
   validates :facebook_url, url: true, allow_blank: true
   validates :instagram_url, url: true, allow_blank: true
   validates :donation_url, url: true, allow_blank: true
-
-  private
-
-  def normalize_phone
-    self.phone_number = Phonelib.parse(phone_number).full_e164.presence
-  end
 end
