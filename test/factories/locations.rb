c1f3f8f6e18129f09cf593374f431477e43a1802
mononoken <ken.maeshima@pm.me>
2024-11-23 14:55:26 -0800

    Remove old comment since this is not managed by ActsAsTenant anymore

--- test/factories/locations.rb
+++ test/factories/locations.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :location do
-    # organization assigned by ActsAsTenant
     city_town { Faker::Address.city }
     sequence(:country) { |n| "Country#{n}" }
     province_state { Faker::Address.state }
