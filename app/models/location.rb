854a43eba1d7f2669a4fcb405821abf281c67dad
mononoken <ken.maeshima@pm.me>
2024-11-23 14:25:50 -0800

    Make conditionals for presence to prevent bootstrap forms showing as required

--- app/models/location.rb
+++ app/models/location.rb
@@ -22,13 +22,23 @@ class Location < ApplicationRecord
   with_options(
     length: {maximum: 50, message: "50 characters maximum"}
   ) do |location|
-    location.validates :country, presence: true
-    location.validates :province_state, presence: true
+    location.validates :country, presence: true, if: :province_state_present?
+    location.validates :province_state, presence: true, if: :city_town_present?
     location.validates :city_town, presence: true
   end
 
   # custom error messages for adopter profile validations
   def custom_messages(attribute)
     errors.where(attribute)
+  end
+
+  private
+
+  def province_state_present?
+    province_state.present?
+  end
+
+  def city_town_present?
+    city_town.present?
   end
 end
