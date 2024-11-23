0eb69646f99fa548cc3e07e9cc5fd240847abfc8
mononoken <ken.maeshima@pm.me>
2024-11-23 15:29:06 -0800

    Add tests for helper

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -11,4 +11,20 @@ class ApplicationHelperTest < ActionView::TestCase
       assert_match(/#{organization.slug}/, organization_home_path(organization))
     end
   end
+
+  context "#default_country" do
+    should "return organization's first country" do
+      organization = create(:organization, :with_location)
+
+      assert_equal organization.locations.first.country, default_country(organization)
+    end
+
+    context "when organization has no location" do
+      should "return empty string" do
+        organization = create(:organization)
+
+        assert_equal "", default_country(organization)
+      end
+    end
+  end
 end
