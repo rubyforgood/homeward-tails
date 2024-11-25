19dbb29707a367e81fc49084ec753e31c40af28c
mononoken <ken.maeshima@pm.me>
2024-11-24 18:27:43 -0800

    Reorder rescue in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   verify_authorized unless: :devise_controller?
-
   before_action :set_current_user
   around_action :switch_locale
+
+  rescue_from ActionPolicy::Unauthorized do |ex|
+    flash[:alert] = t("errors.authorization_error")
+
+    redirect_back fallback_location: root_path
+  end
 
   def set_current_user
     Current.user = current_user
@@ -12,11 +17,5 @@ class ApplicationController < ActionController::Base
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
-  end
-
-  rescue_from ActionPolicy::Unauthorized do |ex|
-    flash[:alert] = t("errors.authorization_error")
-
-    redirect_back fallback_location: root_path
   end
 end
