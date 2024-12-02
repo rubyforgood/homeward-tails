bf4c39963496be33fbb7e92fe04ac035cff87e39
Kate Higa <16447748+khiga8@users.noreply.github.com>
2024-12-02 18:28:15 -0500

    undo

--- app/services/organizations/importers/google_csv_import_service.rb
+++ app/services/organizations/importers/google_csv_import_service.rb
@@ -13,41 +13,39 @@ module Organizations
       end
 
       def call
-        # TODO: Check if file is a valid CSV format before we read from it.
-        begin
-          CSV.foreach(@file.to_path, headers: true, skip_blanks: true).with_index(1) do |row, index|
-            # Using Google Form headers
-            email = row["Email"].downcase
-            csv_timestamp = Time.parse(row["Timestamp"])
+        # Validate that file is in CSV format before we read from it.
+        CSV.foreach(@file.to_path, headers: true, skip_blanks: true).with_index(1) do |row, index|
+          # Using Google Form headers
+          email = row["Email"].downcase
+          csv_timestamp = Time.parse(row["Timestamp"])
 
-            person = Person.find_by(email:, organization: @organization)
-            previously_matched_form_submission = FormSubmission.where(person:, csv_timestamp:)
-            if person.nil?
-              @no_match << [index, email]
-            elsif previously_matched_form_submission.present?
-              next
-            else
-              latest_form_submission = person.latest_form_submission
-              ActiveRecord::Base.transaction do
-                # This checks for the empty form submission that is added when a person is created
-                if latest_form_submission.csv_timestamp.nil? && latest_form_submission.form_answers.empty?
-                  latest_form_submission.update!(csv_timestamp:)
-                  create_form_answers(latest_form_submission, row)
-                else
-                  # if the person submits a new/updated form,
-                  # i.e. an additional row in the csv with the same email / different timestamp,
-                  # a new form_submission will be created
-                  create_form_answers(FormSubmission.create!(person:, csv_timestamp:), row)
-                end
-                @count += 1
+          person = Person.find_by(email:, organization: @organization)
+          previously_matched_form_submission = FormSubmission.where(person:, csv_timestamp:)
+
+          if person.nil?
+            @no_match << [index, email]
+          elsif previously_matched_form_submission.present?
+            next
+          else
+            latest_form_submission = person.latest_form_submission
+            ActiveRecord::Base.transaction do
+              # This checks for the empty form submission that is added when a person is created
+              if latest_form_submission.csv_timestamp.nil? && latest_form_submission.form_answers.empty?
+                latest_form_submission.update!(csv_timestamp:)
+                create_form_answers(latest_form_submission, row)
+              else
+                # if the person submits a new/updated form,
+                # i.e. an additional row in the csv with the same email / different timestamp,
+                # a new form_submission will be created
+                create_form_answers(FormSubmission.create!(person:, csv_timestamp:), row)
               end
+              @count += 1
             end
-          rescue
-            debugger
-            @errors << [index, e]
           end
-          Status.new(@errors.empty?, @count, @no_match, @errors)
+        rescue => e
+          @errors << [index, e]
         end
+        Status.new(@errors.empty?, @count, @no_match, @errors)
       end
 
       private
