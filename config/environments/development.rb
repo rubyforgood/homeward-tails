da94b8439efbee86a5b67765f084e77b9f5a1f26
Ben <robinsonbena@gmail.com>
2024-12-05 15:15:13 -0800

    do not open mail automatically in browser

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # per letter-opener gem config to set delivery method
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
   # Don't care if the mailer can't send.
