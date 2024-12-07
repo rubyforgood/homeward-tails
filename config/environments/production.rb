08b8a5924b5db4039723f447542a1e9c051a01a2
Ben <robinsonbena@gmail.com>
2024-12-07 14:34:32 -0800

    reimplement how we handled script name for mailers and fix the production config to remove the trailing / because it was resulting in URLs with two slashes before the slug...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # devise mailer (e.g. reset password)
-  config.action_mailer.default_url_options = {host: "https://www.homewardtails.org/"}
+  config.action_mailer.default_url_options = {host: "https://www.homewardtails.org"}
   config.action_mailer.default_options = {from: "homewardtails@gmail.com"}
 
   # Ignore bad email addresses and do not raise email delivery errors.
