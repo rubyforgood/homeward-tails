0ef4f686848e8f20afd710413bb3c77672e192d6
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-12-10 12:16:57 -0500

    load defaults

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module BajaPetRescue
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
 
     # config.active_record.verify_foreign_keys_for_fixtures = false
 
