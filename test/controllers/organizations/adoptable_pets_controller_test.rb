0fb50523f2eb24bdc81c32f4e1762fe25a7ddc7b
Ben <95949082+kasugaijin@users.noreply.github.com>
2024-12-10 11:45:25 -0800

    Remove adoptable pets policy (#1233)
    * Remove adoptable pets policy as it is an entirely public route
    
    * lint
    
    * fix tests
    
    * update apply button and change copy when pet application_paused is true

--- test/controllers/organizations/adoptable_pets_controller_test.rb
+++ test/controllers/organizations/adoptable_pets_controller_test.rb
@@ -10,14 +10,6 @@ module Organizations
     end
 
     context "#index" do
-      should "have authorized scope" do
-        assert_have_authorized_scope(
-          type: :active_record_relation, with: Organizations::AdoptablePetPolicy
-        ) do
-          get adoptable_pets_url(species: "dog")
-        end
-      end
-
       should "assign only unadopted pets" do
         adopted_pet = create(:pet, :adopted, species: "Dog")
 
@@ -36,14 +28,6 @@ module Organizations
         assert_select(".card li:nth-of-type(3)", text: "Breed: #{@pet.breed}")
         assert_select(".card li:nth-of-type(4)", text: "Sex: #{@pet.sex}")
         assert_select(".card li:nth-of-type(5)", text: "Weight range: #{@pet.weight_from}-#{@pet.weight_to} #{@pet.weight_unit}")
-      end
-    end
-
-    context "#show" do
-      should "be authorized" do
-        assert_authorized_to(:show?, @pet, with: Organizations::AdoptablePetPolicy) do
-          get adoptable_pet_url(@pet)
-        end
       end
     end
   end
