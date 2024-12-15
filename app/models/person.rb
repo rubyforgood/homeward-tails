e89cad3f3c4cff550f336aaac087aa665eaefbec
Ben <95949082+kasugaijin@users.noreply.github.com>
2024-12-15 08:11:08 -0800

    Form submission refactor (#1244)
    * remove broadcast as it throws redis missing error in production; add dependent destroy on org
    
    * Update associations and add migration to move adopter applications to belong to person
    
    * Update AdopterApplication to use Person; update seeds for new associations
    
    * refactor models, controllers, views, tests to handle new associations
    
    * remove creation of form submission on adopter sign up
    
    * implemenet routs, controllers, views and policies for staff to review form submissions and form answers;
    
    * lint
    
    * appease brakeman
    
    * add tests; add form answer header
    
    * update tests
    
    * lint
    
    * remove handling the nil person case, this should fail loudly as it should not occur
    
    * remove partial as I am not using it
    
    * move empty state text to translation file
    
    * fix
    
    * update foster links to fosterer data
    
    * Update to show csv_timestamp and import date and show HH MM on the submitted at in case someone submits a form twice in one day, then it is easiser to determine which answers you are looking at
    
    * merge main
    
    * remove creation of formsubmission on creation of adopter user in seeds
    
    * lint
    
    * add remaining translations

--- app/models/person.rb
+++ app/models/person.rb
@@ -29,10 +29,10 @@ class Person < ApplicationRecord
   has_one :latest_form_submission, -> { order(created_at: :desc) }, class_name: "FormSubmission"
   has_many :form_submissions, dependent: :destroy
   has_many :form_answers, through: :form_submissions
-  has_many :adopter_applications, through: :form_submissions
+  has_many :adopter_applications, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_pets, through: :likes, source: :pet
-  has_one :location, as: :locatable
+  has_one :location, as: :locatable, dependent: :destroy
   accepts_nested_attributes_for :location,
     reject_if: ->(attributes) { attributes["city_town"].blank? }
   has_many :matches # , dependent: :destroy
---
+++ test/policies/organizations/form_submission_policy_test.rb
@@ -0,0 +1,323 @@
+require "test_helper"
+
+# See https://actionpolicy.evilmartians.io/#/testing?id=testing-policies
+module Organizations
+  class FormSubmissionPolicyTest < ActiveSupport::TestCase
+    include PetRescue::PolicyAssertions
+
+    context "context only action" do
+      setup do
+        @organization = ActsAsTenant.current_tenant
+        @policy = -> {
+          Organizations::FormSubmissionPolicy.new(FormSubmission, user: @user,
+            organization: @organization)
+        }
+      end
+
+      context "#manage?" do
+        setup do
+          @action = -> { @policy.call.apply(:manage?) }
+        end
+
+        context "when user is nil" do
+          setup do
+            @user = nil
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is adopter" do
+          setup do
+            @user = create(:adopter)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is fosterer" do
+          setup do
+            @user = create(:fosterer)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is activated admin" do
+          setup do
+            @user = create(:admin)
+          end
+
+          context "when organization context is a different organization" do
+            setup do
+              @organization = create(:organization)
+            end
+
+            should "return false" do
+              assert_equal false, @action.call
+            end
+          end
+
+          context "when organization context is admin organization" do
+            should "return true" do
+              assert_equal true, @action.call
+            end
+          end
+        end
+
+        context "when user is deactivated admin" do
+          setup do
+            @user = create(:admin, :deactivated)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is superadmin" do
+          setup do
+            @user = create(:super_admin)
+          end
+
+          context "when organization context is a different organization" do
+            setup do
+              @organization = create(:organization)
+            end
+
+            should "return false" do
+              assert_equal false, @action.call
+            end
+          end
+
+          context "when organization context is superadmin organization" do
+            should "return true" do
+              assert_equal true, @action.call
+            end
+          end
+        end
+      end
+
+      context "#index?" do
+        should "be an alias to :manage?" do
+          assert_alias_rule @policy.call, :index?, :manage?
+        end
+      end
+    end
+
+    context "existing record action" do
+      setup do
+        @form_submission = create(:form_submission)
+        @policy = -> {
+          Organizations::FormSubmissionPolicy.new(@form_submission, user: @user)
+        }
+      end
+
+      context "#manage?" do
+        setup do
+          @action = -> { @policy.call.apply(:manage?) }
+        end
+
+        context "when user is nil" do
+          setup do
+            @user = nil
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is adopter" do
+          setup do
+            @user = create(:adopter)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is fosterer" do
+          setup do
+            @user = create(:fosterer)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is activated admin" do
+          setup do
+            @user = create(:admin)
+          end
+
+          context "when FormSubmission belongs to a different organization" do
+            setup do
+              ActsAsTenant.with_tenant(create(:organization)) do
+                @form_submission = create(:form_submission)
+              end
+            end
+
+            should "return false" do
+              assert_equal false, @action.call
+            end
+          end
+
+          context "when FAQ belongs to admin organization" do
+            should "return true" do
+              assert_equal true, @action.call
+            end
+          end
+        end
+
+        context "when user is deactivated admin" do
+          setup do
+            @user = create(:admin, :deactivated)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is admin" do
+          setup do
+            @user = create(:super_admin)
+          end
+
+          context "when FormSubmission belongs to a different organization" do
+            setup do
+              ActsAsTenant.with_tenant(create(:organization)) do
+                @form_submission = create(:form_submission)
+              end
+            end
+
+            should "return false" do
+              assert_equal false, @action.call
+            end
+          end
+
+          context "when FormSubmission belongs to admin organization" do
+            should "return true" do
+              assert_equal true, @action.call
+            end
+          end
+        end
+      end
+    end
+
+    context "existing record action" do
+      setup do
+        @form_submission = create(:form_submission)
+        @form_answer = create(:form_answer, form_submission: @form_submission)
+        @policy = -> {
+          Organizations::FormSubmissionPolicy.new(@form_answer, user: @user)
+        }
+      end
+
+      context "#manage?" do
+        setup do
+          @action = -> { @policy.call.apply(:manage?) }
+        end
+
+        context "when user is nil" do
+          setup do
+            @user = nil
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is adopter" do
+          setup do
+            @user = create(:adopter)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is fosterer" do
+          setup do
+            @user = create(:fosterer)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is activated admin" do
+          setup do
+            @user = create(:admin)
+          end
+
+          context "when FormSubmission belongs to a different organization" do
+            setup do
+              ActsAsTenant.with_tenant(create(:organization)) do
+                @form_submission = create(:form_submission)
+                @form_answer = create(:form_answer, form_submission: @form_submission)
+              end
+            end
+
+            should "return false" do
+              assert_equal false, @action.call
+            end
+          end
+
+          context "when FAQ belongs to admin organization" do
+            should "return true" do
+              assert_equal true, @action.call
+            end
+          end
+        end
+
+        context "when user is deactivated admin" do
+          setup do
+            @user = create(:admin, :deactivated)
+          end
+
+          should "return false" do
+            assert_equal false, @action.call
+          end
+        end
+
+        context "when user is admin" do
+          setup do
+            @user = create(:super_admin)
+          end
+
+          context "when FormSubmission belongs to a different organization" do
+            setup do
+              ActsAsTenant.with_tenant(create(:organization)) do
+                @form_submission = create(:form_submission)
+                @form_answer = create(:form_answer, form_submission: @form_submission)
+              end
+            end
+
+            should "return false" do
+              assert_equal false, @action.call
+            end
+          end
+
+          context "when FormSubmission belongs to admin organization" do
+            should "return true" do
+              assert_equal true, @action.call
+            end
+          end
+        end
+      end
+    end
+  end
+end
