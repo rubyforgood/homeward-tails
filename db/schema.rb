b4ca29fab2722757727e51b511dc3613fc138d80
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-12-15 17:33:59 -0500

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_11_182208) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_11_182208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
