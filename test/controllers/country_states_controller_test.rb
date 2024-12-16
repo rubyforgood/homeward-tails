0e4dd67df19eb807e7bd2c209a62a7e878b96faa
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-12-15 22:51:13 -0500

    Controller test for country_state_controller

---
+++ test/controllers/country_states_controller_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class CountryStatesControllerTest < ActionDispatch::IntegrationTest
+  test "should return turbo stream with the states in it" do
+    adopter = create(:adopter)
+    sign_in adopter
+
+    name = "adopter[address_attributes][state]"
+    target = "adopter_foster_profile_location_attributes_province_state"
+
+    get country_states_path,
+      headers: {"Accept" => "text/vnd.turbo-stream.html"},
+      params: {country: "CA", target:, name:, province_state: "BC"}
+
+    assert_response :success
+    assert_select "turbo-stream[action='replace'][target='#{target}']" do
+      assert_select "template" do
+        assert_select "select[name='#{name}']" do
+          assert_select "option", 13
+        end
+      end
+    end
+  end
+end
