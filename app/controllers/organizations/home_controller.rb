591588ee6bdb76c08db59ef672886fb96e64fb66
Ben <95949082+kasugaijin@users.noreply.github.com>
2024-12-17 07:46:19 -0800

    Small fixes (#1266)
    * if logo does not meet validation error is thrown
    
    * fix devise forms to be more uniform
    
    * add logo to top navbar and fix its positioning relative to org name
    
    * update form
    
    * fix featured pets to have same image heights and handle collections of unadopted pets smaller than 4
    
    * lint
    
    * fix test

--- app/controllers/organizations/home_controller.rb
+++ app/controllers/organizations/home_controller.rb
@@ -4,8 +4,8 @@ class Organizations::HomeController < Organizations::BaseController
 
   def index
     @pets = Pet.with_photo
+      .unadopted
       .includes(images_attachments: :blob)
-      .where(organization: Current.organization)
       .sample(4)
   end
 end
