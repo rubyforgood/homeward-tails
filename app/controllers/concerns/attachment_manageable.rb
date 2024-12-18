65bf4b9dfd63e43513883c71f25a06897f92f209
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2024-12-17 20:26:53 -0500

    fix: adjust equality

--- app/controllers/concerns/attachment_manageable.rb
+++ app/controllers/concerns/attachment_manageable.rb
@@ -2,14 +2,14 @@ module AttachmentManageable
   def allow_only_one_attachment
     return unless params[:files].is_a?(Array)
 
-    flash.now[:alert] = t(".attachment_manageable.upload_limit_exceeded")
+    flash.now[:alert] = t("attachment_manageable.upload_limit_exceeded")
     render turbo_stream: turbo_stream.replace("flash", partial: "layouts/shared/flash_messages")
   end
   
   def alert_for_no_images
-    if params[:action] === 'attach_images' and params[:pet][:images].length > 1
+    if params[:action] == 'attach_images' and params[:pet][:images].length > 1
       return
-    elsif params[:action] === 'attach_files' and params[:pet][:files].length > 1
+    elsif params[:action] == 'attach_files' and params[:pet][:files].length > 1
       return
     else
       flash.now[:alert] = t("attachment_manageable.attachment_missing")
