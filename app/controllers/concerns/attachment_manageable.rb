b4dc0f8bfb08dc009ad28456458abb3c23067c09
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2024-12-18 20:45:05 -0500

    fix: broken tests, rename variables and method

--- app/controllers/concerns/attachment_manageable.rb
+++ app/controllers/concerns/attachment_manageable.rb
@@ -5,12 +5,18 @@ module AttachmentManageable
     flash.now[:alert] = t("attachment_manageable.upload_limit_exceeded")
     render turbo_stream: turbo_stream.replace("flash", partial: "layouts/shared/flash_messages")
   end
-  
-  def alert_for_no_images
-    if params[:action] == 'attach_images' and params[:pet][:images].length > 1
-      return
-    elsif params[:action] == 'attach_files' and params[:pet][:files].length > 1
-      return
+
+  def show_alert_if_attachment_missing
+    if !params[:pet][:images].nil?
+      no_empty_images = params[:pet][:images].reject { |image| image == "" }
+    elsif !params[:pet][:files].nil?
+      no_empty_files = params[:pet][:files].reject { |image| image == "" }
+    end
+
+    if params[:action] == "attach_images" && no_empty_images.length >= 1
+      nil
+    elsif params[:action] == "attach_files" && no_empty_files.length >= 1
+      nil
     else
       flash.now[:alert] = t("attachment_manageable.attachment_missing")
       render turbo_stream: turbo_stream.replace("flash", partial: "layouts/shared/flash_messages")
--- app/controllers/organizations/staff/pets_controller.rb
+++ app/controllers/organizations/staff/pets_controller.rb
@@ -1,6 +1,6 @@
 class Organizations::Staff::PetsController < Organizations::BaseController
-  before_action :alert_for_no_images, only: [:attach_images, :attach_files]
   before_action :set_pet, only: [:show, :edit, :update, :destroy, :attach_images, :attach_files]
+  before_action :show_alert_if_attachment_missing, only: [:attach_images, :attach_files]
   include ::Pagy::Backend
   include AttachmentManageable
 
