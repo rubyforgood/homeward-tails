c0f3c3d8a51316a418e48024e78c50b65b97c785
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-12-20 09:16:46 -0500

    test index and create

--- test/controllers/organizations/staff/external_form_upload_controller_test.rb
+++ test/controllers/organizations/staff/external_form_upload_controller_test.rb
@@ -3,55 +3,25 @@ require "test_helper"
 module Organizations
   module Staff
     class ExternalFormUploadControllerTest < ActionDispatch::IntegrationTest
-      context "success" do
-        setup do
-          file = fixture_file_upload("google_form_sample.csv", "text/csv")
-          @params = {files: file}
-          admin = create(:admin)
-          @adopter = create(:adopter, email: "adopter1111@alta.com")
-          @adopter2 = create(:adopter, email: "no_answer_will_be_created@alta.com")
-          sign_in admin
-        end
-
-        should "Creates new form submission" do
-          assert_difference "@adopter.person.form_submissions.count" do
-            post staff_external_form_upload_index_path, params: @params
-          end
-        end
-
-        should "It does not create form answers for adopter2" do
-          assert_no_difference "@adopter2.person.form_submissions.count" do
-            post staff_external_form_upload_index_path, params: @params
-          end
-        end
-
-        should "shows success feedback" do
-          post staff_external_form_upload_index_path, params: @params, as: :turbo_stream
-
-          assert_response :success
-          assert_turbo_stream(action: "replace", count: 1) do
-            assert_select "h5", text: "File successfully scanned"
-          end
-        end
+      setup do
+        file = fixture_file_upload("google_form_sample.csv", "text/csv")
+        @params = {files: file}
+        admin = create(:admin)
+        sign_in admin
       end
 
-      context "error" do
-        setup do
-          file = fixture_file_upload("google_form_error_sample.csv", "text/csv")
-          @params = {files: file}
-          admin = create(:admin)
-          create(:adopter, email: "adopter1234@alta.com")
-          sign_in admin
-        end
+      should "get index" do
+        get staff_external_form_upload_index_path
+        assert_response :success
+      end
 
-        should "shows error feedback" do
+      should "Creates new form submission" do
+        assert_enqueued_with(job: CsvImportJob) do
           post staff_external_form_upload_index_path, params: @params, as: :turbo_stream
-
-          assert_response :success
-          assert_turbo_stream(action: "replace", count: 1) do
-            assert_select "h5", text: "File scanned: 1 error(s) present"
-          end
         end
+
+        assert_response :success
+        assert_equal "File uploaded for processing", flash[:notice]
       end
     end
   end
