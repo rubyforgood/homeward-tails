3b5823a859e8683ee94ef786b909462e93ff4aa4
Justin <16829344+jmilljr24@users.noreply.github.com>
2024-12-20 11:48:41 -0500

    Csv background job (#1281)
    * adjust service to handle blob
    
    * broadcast completion
    
    * add loading spinnder
    
    * i18n and clean up
    
    * wording
    
    * turbo frame not needed
    
    * fix broadcast / minor tweaks
    
    * change change to current user
    
    get current org in service
    
    * set org with actsastenant
    
    * update service tests
    
    * test index and create
    
    * clean up
    
    * assert errors instance variable
    
    * proper order
    
    * standard

--- app/services/organizations/importers/csv_import_service.rb
+++ app/services/organizations/importers/csv_import_service.rb
@@ -4,9 +4,10 @@ module Organizations
   module Importers
     class CsvImportService
       Status = Data.define(:success?, :count, :no_match, :errors)
-      def initialize(file)
+      def initialize(file, current_user_id)
         @file = file
-        @organization = Current.organization
+        @organization = ActsAsTenant.current_tenant
+        @current_user = User.find(current_user_id)
         @count = 0
         @no_match = []
         @errors = []
@@ -14,51 +15,55 @@ module Organizations
 
       def call
         catch(:halt_import) do
-          validate_file
+          @file.download do |data|
+            validate_file(data)
+            CSV.parse(data, headers: true, skip_blanks: true).each_with_index do |row, index|
+              # Header may be different depending on which form applicaiton was used(e.g. google forms) or how it was created(User creates form with "Email Address")
+              email = row[@email_header].downcase
+              # Google forms uses "Timestamp", other services may use a different header
+              csv_timestamp = Time.parse(row["Timestamp"]) if row["Timestamp"].present?
 
-          CSV.foreach(@file.to_path, headers: true, skip_blanks: true).with_index(2) do |row, index|
-            # Header may be different depending on which form applicaiton was used(e.g. google forms) or how it was created(User creates form with "Email Address")
-            email = row[@email_header].downcase
-            # Google forms uses "Timestamp", other services may use a different header
-            csv_timestamp = Time.parse(row["Timestamp"]) if row["Timestamp"].present?
+              person = Person.find_by(email:, organization: @organization)
+              previously_matched_form_submission = FormSubmission.where(person:, csv_timestamp:)
 
-            person = Person.find_by(email:, organization: @organization)
-            previously_matched_form_submission = FormSubmission.where(person:, csv_timestamp:)
-
-            if person.nil?
-              @no_match << [index, email]
-            elsif previously_matched_form_submission.present?
-              next
-            else
-              ActiveRecord::Base.transaction do
-                create_form_answers(FormSubmission.create!(person:, csv_timestamp:), row)
-                @count += 1
+              if person.nil?
+                @no_match << [index + 2, email]
+              elsif previously_matched_form_submission.present?
+                next
+              else
+                ActiveRecord::Base.transaction do
+                  create_form_answers(FormSubmission.create!(person:, csv_timestamp:), row)
+                  @count += 1
+                end
               end
+            rescue => e
+              @errors << [index + 2, e]
             end
-          rescue => e
-            @errors << [index, e]
           end
         end
-        Status.new(@errors.empty?, @count, @no_match, @errors)
+        Turbo::StreamsChannel.broadcast_replace_to ["csv_import", @current_user],
+          target: "results",
+          partial: "organizations/staff/external_form_upload/upload_results",
+          locals: {
+            import: Status.new(@errors.empty?, @count, @no_match, @errors)
+          }
       end
 
       private
 
-      def validate_file
+      def validate_file(data)
         raise FileTypeError unless @file.content_type == "text/csv"
-
-        first_row = CSV.foreach(@file.to_path).first
-        raise FileEmptyError if first_row.nil?
+        first_row = CSV.new(data).shift
 
         raise TimestampColumnError unless first_row.include?("Timestamp")
 
         email_headers = ["Email", "email", "Email Address", "email address"]
-        email_headers.each do |e|
-          @email_header = e if first_row.include?(e)
+        email_headers.each do |header|
+          @email_header = header if first_row.include?(header)
         end
         raise EmailColumnError unless @email_header
-      rescue FileTypeError, FileEmptyError, TimestampColumnError, EmailColumnError => e
-        @errors << e
+      rescue FileTypeError, TimestampColumnError, EmailColumnError => e
+        @errors << [1, e]
         throw :halt_import
       end
 
