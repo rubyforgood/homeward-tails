2ee09d5a75a9af9ea95ffab84d9adfea337b789e
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2024-12-26 14:53:11 -0500

    feat: add alert when missing attachment #1220 (#1273)
    * feat: add alert when missing attachment
    
    * fix: adjust equality
    
    * fix: broken tests, rename variables and method
    
    * refactor: use present? instead of nil?
    
    * refactor: change no_empty_images to images and files
    
    * chore: add comment on why we remove empty string  from files / images array

--- app/controllers/concerns/attachment_manageable.rb
+++ app/controllers/concerns/attachment_manageable.rb
@@ -2,7 +2,26 @@ module AttachmentManageable
   def allow_only_one_attachment
     return unless params[:files].is_a?(Array)
 
-    flash.now[:alert] = t(".attachment_manageable.upload_limit_exceeded")
+    flash.now[:alert] = t("attachment_manageable.upload_limit_exceeded")
     render turbo_stream: turbo_stream.replace("flash", partial: "layouts/shared/flash_messages")
+  end
+
+  def show_alert_if_attachment_missing
+    # images and files are arrays that sometimes populated with empty strings, so we want to remove
+    # those empty strings before checking whether or no "real" images / files are being attached
+    if params[:pet][:images].present?
+      images = params[:pet][:images].reject { |image| image == "" }
+    elsif params[:pet][:files].present?
+      files = params[:pet][:files].reject { |file| file == "" }
+    end
+
+    if params[:action] == "attach_images" && images.length >= 1
+      nil
+    elsif params[:action] == "attach_files" && files.length >= 1
+      nil
+    else
+      flash.now[:alert] = t("attachment_manageable.attachment_missing")
+      render turbo_stream: turbo_stream.replace("flash", partial: "layouts/shared/flash_messages")
+    end
   end
 end
