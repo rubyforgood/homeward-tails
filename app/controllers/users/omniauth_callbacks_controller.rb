4d951bb7fce1dbc26cb4c36a987331be93ba7435
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2025-01-01 22:01:49 -0500

    Comment for setting oauth and nesting modules

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,20 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  include OrganizationScopable
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    include OrganizationScopable
 
-  def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    def google_oauth2
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      end
     end
-  end
 
-  def failure
-    redirect_to root_path
+    def failure
+      redirect_to root_path
+    end
   end
 end
