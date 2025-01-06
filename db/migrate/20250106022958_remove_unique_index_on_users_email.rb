7da7b92d64cc0879d17aa99b6b712d36ae0a0ef3
Ben <robinsonbena@gmail.com>
2025-01-05 18:51:34 -0800

    lint

--- db/migrate/20250106022958_remove_unique_index_on_users_email.rb
+++ db/migrate/20250106022958_remove_unique_index_on_users_email.rb
@@ -2,7 +2,7 @@ class RemoveUniqueIndexOnUsersEmail < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-      remove_index :users, name: "index_users_on_email"
-      add_index :users, :email, algorithm: :concurrently
+    remove_index :users, name: "index_users_on_email"
+    add_index :users, :email, algorithm: :concurrently
   end
 end
