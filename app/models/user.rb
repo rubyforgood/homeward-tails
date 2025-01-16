01244c413a068a2d375c35f15ea0fb6d5aa1be63
mononoken <81536479+mononoken@users.noreply.github.com>
2025-01-15 20:58:27 -0800

    Uncomment tos validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,8 +65,8 @@ class User < ApplicationRecord
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   }
   validate :prevent_email_change, on: :update
-  # validates :tos_agreement, acceptance: {message: "Please accept the Terms and Conditions"},
-  #   allow_nil: false, on: :create
+  validates :tos_agreement, acceptance: {message: "Please accept the Terms and Conditions"},
+    allow_nil: false, on: :create
 
   belongs_to :person
   accepts_nested_attributes_for :person
