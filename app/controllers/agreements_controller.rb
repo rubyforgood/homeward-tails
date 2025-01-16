6efb95eb65a034d84564076b4d93cb3eef489d5b
mononoken <81536479+mononoken@users.noreply.github.com>
2025-01-15 20:58:27 -0800

    Create agreement controller

---
+++ app/controllers/agreements_controller.rb
@@ -0,0 +1,29 @@
+class AgreementsController < ApplicationController
+  # FIXME: Remove skip
+  skip_verify_authorized only: %i[edit update]
+  before_action :set_user, only: %i[edit update]
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+        redirect_to root_path, notice: "Success"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:tos_agreement)
+  end
+
+  def set_user
+    @user = current_user
+
+    # FIXME: Update authorize
+    # authorize! @user
+  end
+end
