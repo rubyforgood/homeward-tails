b87c6cf70f976b8b051337de9b680ddb8e0d733c
Meri-MG <mgogi13@freeuni.edu.ge>
2025-01-21 15:24:38 +0400

    Remove the comment

--- app/models/concerns/avatarable.rb
+++ app/models/concerns/avatarable.rb
@@ -6,7 +6,6 @@ module Avatarable
     # forces Azure to be defined
     has_one_attached :avatar unless ENV["PRECOMPILING"]
 
-    # TODO:  move these validation strings to a locale file
     validates :avatar,
       content_type: {in: ["image/png", "image/jpeg"], message: I18n.t(".content_type_invalid")},
       size: {less_than: 1.megabytes, message: :file_size_not_less_than}
