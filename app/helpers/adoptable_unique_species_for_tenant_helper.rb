8d7e971b49df05d452a42c0d68166c7177541153
Jason Wang <jason.wang7517@gmail.com>
2025-01-28 20:14:03 -0800

    adding helper function

---
+++ app/helpers/adoptable_unique_species_for_tenant_helper.rb
@@ -0,0 +1,9 @@
+module AdoptableUniqueSpeciesForTenantHelper
+  def adoptable_unique_species(organization)
+    organization.pets
+                .unadopted
+                .where(placement_type: ["Adoptable", "Adoptable and Fosterable"])
+                .distinct.
+                pluck(:species)
+  end
+end 
