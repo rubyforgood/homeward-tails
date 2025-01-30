ce24ea22b2d99846a8577da38a424ab9f51d7b29
Jason Wang <jason.wang7517@gmail.com>
2025-01-29 22:49:18 -0800

    removing instance variable from partial

--- app/helpers/adoptable_unique_species_for_tenant_helper.rb
+++ app/helpers/adoptable_unique_species_for_tenant_helper.rb
@@ -1,3 +1,4 @@
+# Used in the navbar since the navbar isn't scoped to a specific tenant, requiring us to set a tenant to use the existing scope.
 module AdoptableUniqueSpeciesForTenantHelper
   def adoptable_unique_species(organization)
     organization.pets.adoptable_unique_species
