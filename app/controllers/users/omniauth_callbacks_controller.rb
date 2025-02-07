f7abe21fd8da8519f15294a0845a618755909631
mononoken <81536479+mononoken@users.noreply.github.com>
2025-02-06 22:04:38 -0800

    Move flash setting before redirect

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,12 +6,12 @@ module Users
       @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
         sign_in @user, event: :authentication
+        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
         if !@user.tos_agreement
           redirect_to edit_tos_agreement_path
         else
           redirect_to root_path
         end
-        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
