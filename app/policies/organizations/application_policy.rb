54de7a6df7ad75859c5224f047c70eebf2646b33
mononoken <81536479+mononoken@users.noreply.github.com>
2025-02-06 20:34:26 -0800

    Move tos agreement verification to main application policy

--- app/policies/organizations/application_policy.rb
+++
@@ -1,7 +0,0 @@
-class Organizations::ApplicationPolicy < ApplicationPolicy
-  pre_check :verify_tos_agreement!
-
-  def verify_tos_agreement!
-    deny!(:no_tos_accepted) unless user.tos_agreement?
-  end
-end
