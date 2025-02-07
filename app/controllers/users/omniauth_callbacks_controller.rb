4daee73003d214b36c4039a07d465c299fc84ba2
mononoken <81536479+mononoken@users.noreply.github.com>
2025-02-06 21:56:41 -0800

    Redirect conditionally instead for oauth users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,11 @@ module Users
       @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
         sign_in @user, event: :authentication
-        redirect_to edit_tos_agreement_path
+        if @user.tos_agreement
+          redirect_to root_path
+        else
+          redirect_to edit_tos_agreement_path
+        end
         set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
