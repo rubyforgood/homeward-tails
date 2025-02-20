6c39909f8b04b32d16859a0dc57e13373ef29621
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2025-02-20 18:55:36 -0500

    Test cases for Note model

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = create(:adopter)
+    @notable = @user.person
+    @note = Note.new(notable: @notable, notes: "This is a test note")
+  end
+
+  test "should be valid" do
+    assert @note.valid?
+  end
+
+  test "should require a notable" do
+    @note.notable = nil
+    assert_not @note.valid?
+  end
+
+  test "should allow different notable types" do
+    @application = create(:adopter_application, person: @user.person)
+    note_for_application = Note.new(notable: @application, notes: "Application note")
+    assert note_for_application.valid?
+  end
 end
