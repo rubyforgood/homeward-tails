f2f222cfbee76348f01db384da5e99988cd70cef
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-04-25 09:03:48 -0400

    change group factory to use find or create

--- test/factories/groups.rb
+++ test/factories/groups.rb
@@ -17,5 +17,16 @@ FactoryBot.define do
     trait :super_admin do
       name { "super_admin" }
     end
+
+    # Override to_create to use find_or_create_by
+    to_create do |instance|
+      found = Group.find_by(name: instance.name, organization_id: instance.organization_id)
+      if found
+        instance.id = found.id
+        instance.instance_variable_set(:@new_record, false)
+      else
+        instance.save!
+      end
+    end
   end
 end
