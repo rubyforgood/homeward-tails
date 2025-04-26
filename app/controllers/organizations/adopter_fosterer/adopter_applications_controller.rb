84c9d49f4743a3338ab1397f4ca69e808fb7e3d4
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-04-26 09:37:31 -0400

    revert
    needs more work to change to module

--- app/controllers/organizations/adopter_fosterer/adopter_applications_controller.rb
+++ app/controllers/organizations/adopter_fosterer/adopter_applications_controller.rb
@@ -1,61 +1,57 @@
-module Organizations
-  module AdopterFosterer
-    class AdopterApplicationsController < Organizations::BaseController
-      before_action :set_application, only: %i[update]
-      layout "adopter_foster_dashboard"
+class Organizations::AdopterFosterer::AdopterApplicationsController < Organizations::BaseController
+  before_action :set_application, only: %i[update]
+  layout "adopter_foster_dashboard"
 
-      def index
-        authorize! with: AdopterApplicationPolicy
+  def index
+    authorize! with: AdopterApplicationPolicy
 
-        @applications = authorized_scope(AdopterApplication.where(profile_show: true), with: AdopterApplicationPolicy)
-      end
+    @applications = authorized_scope(AdopterApplication.where(profile_show: true), with: AdopterApplicationPolicy)
+  end
 
-      def create
-        @pet = Pet.find(application_params[:pet_id])
-        authorize! context: {pet: @pet}, with: AdopterApplicationPolicy
+  def create
+    @pet = Pet.find(application_params[:pet_id])
+    authorize! context: {pet: @pet}, with: AdopterApplicationPolicy
 
-        @application = AdopterApplication.new(application_params)
+    @application = AdopterApplication.new(application_params)
 
-        if @application.save
-          redirect_to adoptable_pet_path(@application.pet),
-            notice: t(".success", message: MessagesHelper.affirmations.sample)
+    if @application.save
+      redirect_to adoptable_pet_path(@application.pet),
+        notice: t(".success", message: MessagesHelper.affirmations.sample)
 
-          # mailer
-          @org_staff = Person.active_staff
-          StaffApplicationNotificationMailer.with(pet: @pet,
-            organization_staff: @org_staff)
-            .new_adoption_application.deliver_now
-        else
-          redirect_to adoptable_pet_path(@pet),
-            alert: t(".error")
-        end
-      end
-
-      # update :status to 'withdrawn' or :profile_show to false
-      def update
-        if @application.update(application_params)
-          redirect_to adopter_fosterer_adopter_applications_path
-        else
-          redirect_to adopter_fosterer_profile_path, alert: t(".error")
-        end
-      end
-
-      private
-
-      def set_application
-        @application = AdopterApplication.find(params[:id])
-        authorize! @application, with: AdopterApplicationPolicy
-      end
-
-      def application_params
-        params.require(:adopter_application).permit(
-          :pet_id,
-          :form_submission_id,
-          :status,
-          :profile_show,
-          :person_id
-        )
-      end
+      # mailer
+      @org_staff = Person.active_staff
+      StaffApplicationNotificationMailer.with(pet: @pet,
+        organization_staff: @org_staff)
+        .new_adoption_application.deliver_now
+    else
+      redirect_to adoptable_pet_path(@pet),
+        alert: t(".error")
     end
+  end
+
+  # update :status to 'withdrawn' or :profile_show to false
+  def update
+    if @application.update(application_params)
+      redirect_to adopter_fosterer_adopter_applications_path
+    else
+      redirect_to adopter_fosterer_profile_path, alert: t(".error")
+    end
+  end
+
+  private
+
+  def set_application
+    @application = AdopterApplication.find(params[:id])
+    authorize! @application, with: AdopterApplicationPolicy
+  end
+
+  def application_params
+    params.require(:adopter_application).permit(
+      :pet_id,
+      :form_submission_id,
+      :status,
+      :profile_show,
+      :person_id
+    )
   end
 end
