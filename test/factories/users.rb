d1ae11ea5447fc3811dc5932859752a7a7271c68
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-04-26 07:24:47 -0400

    change user factories
    fix

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     tos_agreement { true }
-    # deactivated_at { nil }
 
     # after(:create) do |user, _context|
     #   create(:person, user: user, first_name: user.first_name, last_name: user.last_name, email: user.email, user_id: user.id)
@@ -22,38 +21,26 @@ FactoryBot.define do
       end
     end
 
-    # trait :deactivated do
-    #   deactivated_at { DateTime.now }
-    # end
-
-    factory :adopter do
-      after(:create) do |user, _context|
+    trait :adopter do
+      after(:create) do |user|
         user.add_role(:adopter, ActsAsTenant.current_tenant)
-        create(:form_submission, person: Person.where(user_id: user.id).first)
       end
     end
 
-    factory :fosterer do
-      after(:build) do |user, _context|
+    trait :fosterer do
+      after(:create) do |user|
         user.add_role(:fosterer, ActsAsTenant.current_tenant)
       end
     end
 
-    factory :adopter_fosterer do
-      after(:build) do |user, _context|
-        user.add_role(:adopter, ActsAsTenant.current_tenant)
-        user.add_role(:fosterer, ActsAsTenant.current_tenant)
-      end
-    end
-
-    factory :admin do
-      after(:build) do |user, _context|
+    trait :admin do
+      after(:create) do |user|
         user.add_role(:admin, ActsAsTenant.current_tenant)
       end
     end
 
-    factory :super_admin do
-      after(:build) do |user, _context|
+    trait :super_admin do
+      after(:create) do |user|
         user.add_role(:super_admin, ActsAsTenant.current_tenant)
       end
     end
