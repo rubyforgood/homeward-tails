7d891cf56eb2a7921fa1d57c84e998432ab104ab
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2025-04-26 00:29:33 -0400

    Removed unwanted files

--- app/helpers/organizations/staff/notes_helper.rb
+++
@@ -1,2 +0,0 @@
-module Organizations::Staff::NotesHelper
-end
--- test/controllers/organizations/staff/notes_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Organizations::Staff::NotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
