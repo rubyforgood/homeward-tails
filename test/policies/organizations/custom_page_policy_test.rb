4d867b8ee270ac692647192a411c979bf283b31e
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-05-16 18:23:07 -0400

    update custom page policy test

--- test/policies/organizations/custom_page_policy_test.rb
+++ test/policies/organizations/custom_page_policy_test.rb
@@ -5,10 +5,8 @@ class Organizations::CustomPagePolicyTest < ActiveSupport::TestCase
   include PetRescue::PolicyAssertions
 
   setup do
-    @organization = ActsAsTenant.current_tenant
     @policy = -> {
-      Organizations::CustomPagePolicy.new(Pet, user: @user,
-        organization: @organization)
+      Organizations::CustomPagePolicy.new(Pet, person: @person, user: @person&.user)
     }
   end
 
@@ -19,7 +17,7 @@ class Organizations::CustomPagePolicyTest < ActiveSupport::TestCase
 
     context "when user is nil" do
       setup do
-        @user = nil
+        @person = nil
       end
 
       should "return false" do
@@ -29,7 +27,7 @@ class Organizations::CustomPagePolicyTest < ActiveSupport::TestCase
 
     context "when user is adopter" do
       setup do
-        @user = create(:adopter)
+        @person = create(:person, :adopter)
       end
 
       should "return false" do
@@ -39,41 +37,41 @@ class Organizations::CustomPagePolicyTest < ActiveSupport::TestCase
 
     context "when user is fosterer" do
       setup do
-        @user = create(:fosterer)
+        @person = create(:person, :fosterer)
       end
 
       should "return false" do
         assert_equal false, @action.call
+      end
+    end
+
+    context "when user is admin" do
+      setup do
+        @person = create(:person, :admin)
+      end
+
+      should "return false" do
+        assert_equal false, @action.call
+      end
+    end
+
+    context "when user is super admin" do
+      setup do
+        @person = create(:person, :super_admin)
+      end
+
+      should "return true" do
+        assert_equal true, @action.call
       end
     end
 
     context "when user is deactivated staff" do
       setup do
-        @user = create(:admin, :deactivated)
+        @person = create(:person, :admin, deactivated: true)
       end
 
       should "return false" do
         assert_equal false, @action.call
-      end
-    end
-
-    context "when user is active staff" do
-      setup do
-        @user = create(:admin)
-      end
-
-      should "return false" do
-        assert_equal false, @action.call
-      end
-    end
-
-    context "when user is staff admin" do
-      setup do
-        @user = create(:super_admin)
-      end
-
-      should "return true" do
-        assert_equal true, @action.call
       end
     end
   end
