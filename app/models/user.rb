094e0e48b30156ff6f2f68e909b8cb09414cb205
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-05-18 20:01:00 -0400

    add migration to remove org_id from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,6 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  invited_by_id          :bigint
-#  organization_id        :bigint
 #
 # Indexes
 #
@@ -31,7 +30,6 @@
 #  index_users_on_invitation_token      (invitation_token) UNIQUE
 #  index_users_on_invited_by            (invited_by_type,invited_by_id)
 #  index_users_on_invited_by_id         (invited_by_id)
-#  index_users_on_organization_id       (organization_id)
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_10_140218) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_18_235413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -292,7 +292,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_10_140218) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "tos_agreement"
-    t.bigint "organization_id"
     t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -307,7 +306,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_10_140218) do
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
-    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
