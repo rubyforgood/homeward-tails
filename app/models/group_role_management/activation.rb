dcefb6a7d926c3a72509d65a6ab4810d5c819ce6
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-05-23 16:18:41 -0400

    remove name from activation args

--- app/models/group_role_management/activation.rb
+++ app/models/group_role_management/activation.rb
@@ -1,7 +1,7 @@
 # This is a PORO Model that encapsulates logic for modifying associations on Person/User models
 
 # Methods in this class are delegated to the Person model via the `activation` association.
-# This allows calls like `@person.activate!(:adopter)` to invoke logic defined here.
+# This allows calls like `@person.activate!(@group)` to invoke logic defined here.
 #
 module GroupRoleManagement
   class Activation
@@ -11,30 +11,12 @@ module GroupRoleManagement
       @person = person
     end
 
-    def activate!(group_or_name)
-      person_group = find_person_group!(group_or_name)
-
-      person_group.update!(deactivated_at: nil)
+    def activate!(group)
+      person.person_groups.find_by(group: group).update!(deactivated_at: nil)
     end
 
-    def deactivate!(group_or_name)
-      person_group = find_person_group!(group_or_name)
-
-      person_group.update!(deactivated_at: Time.current)
-    end
-
-    private
-
-    def find_person_group!(group_or_name)
-      group = case group_or_name
-      when Group
-        group_or_name
-      when String, Symbol
-        Group.find_by!(name: group_or_name)
-      else
-        raise ArgumentError, "Expected Group or Group Name, received #{group_or_name.class.name}"
-      end
-      person.person_groups.find_by(group_id: group.id)
+    def deactivate!(group)
+      person.person_groups.find_by(group: group).update!(deactivated_at: Time.current)
     end
   end
 end
