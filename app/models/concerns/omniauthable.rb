e6691a5c280446e7642e270299aa93e362afbf98
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2025-06-07 19:45:42 -0400

    Fixed issue with person and user mapping in google sign in flow (#1436)

--- app/models/concerns/omniauthable.rb
+++ app/models/concerns/omniauthable.rb
@@ -7,10 +7,12 @@ module Omniauthable
 
   class_methods do
     def from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.assign_attributes_from_auth(auth)
-        user.set_adopter_role
+      user = where(provider: auth.provider, uid: auth.uid).first_or_create do |u|
+        u.assign_attributes_from_auth(auth)
       end
+
+      user.set_adopter_role if user.persisted?
+      user
     end
   end
 
