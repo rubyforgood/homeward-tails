b40b87ea75da834a9cf88f4f9f17f7a966192434
crespire <hello@crespire.dev>
2025-07-09 09:20:56 -0400

    Empty state for tasks
    We add an empty state for tasks that primarily uses CSS to leverage the
    `:only-child` psuedo selector.
    
    Additional changes were required for the toggling of empty state when
    toggling tasks, so changes were made to the toggle-completed Stimulus
    controller to add a hide class you can input from HTML, as well as
    checking whether you were toggling all tasks or not to decide if we
    needed the empty state to show up.

--- app/javascript/controllers/toggle_completed_controller.js
+++ app/javascript/controllers/toggle_completed_controller.js
@@ -1,17 +1,26 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class ToggleCompletedController extends Controller {
+  static classes = ["show"];
+
   connect() {
-    this.completedTasks = document.querySelectorAll('.bg-success-subtle');
+    this.completedTasks = document.querySelectorAll('.task-list .bg-success-subtle');
+    this.taskFrames = document.querySelectorAll('.task-list turbo-frame');
+    this.emptyState = document.querySelector('.task-list li.only-child-block')
   }
 
   toggleCompletedTasks() {
     const isChecked = this.element.checked;
     if (isChecked) {
+      this.emptyState.classList.remove(this.showClass);
       this.showCompletedTasks();
     } else {
       this.hideCompletedTasks();
+      if (this.#requiresEmptyState()) {
+        this.emptyState.classList.add(this.showClass);
+      }
     }
+
   }
 
   hideCompletedTasks() {
@@ -24,5 +33,13 @@ export default class ToggleCompletedController extends Controller {
     this.completedTasks.forEach((item) => {
       item.classList.remove('d-none');
     });
+  }
+
+  #requiresEmptyState() {
+    if (this.completedTasks.length == this.taskFrames.length) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
